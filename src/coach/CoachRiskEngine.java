eab87b5d13715a74b19821d402129a7ddfd0b460
Douwe Postmus <d.postmus@epi.umcg.nl>
2011-04-11 07:20:15 -0700

    Edited src/coach/CoachRiskEngine.java via GitHub

--- src/coach/CoachRiskEngine.java
+++ src/coach/CoachRiskEngine.java
@@ -15,9 +15,9 @@ public class CoachRiskEngine extends JFrame implements ActionListener {
 		
 		// Create CoachRiskEngine frame
 		
-		this.setSize(840,520);
+		this.setSize(860,600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setTitle("COACH Risk Engine v1.0");
+		this.setTitle("COACH Risk Engine v1.1");
 		this.setLocation(100,100);
 		inputs = new InputPanel(this);
 		this.add(inputs, BorderLayout.NORTH);
@@ -49,27 +49,42 @@ public class CoachRiskEngine extends JFrame implements ActionListener {
 		
 		int iterations = 10000;
 		int i = 0;
-		int[] output = new int[3];
+		int[] output = new int[5];
 		int[] endpointD = new int[iterations];
 		int[] hospitalization = new int[iterations];
 		int[] numHosp = new int[iterations];
+		int[] endpointH = new int[iterations];
+		int[] endpointHind = new int[iterations];
 		
 		while (i<iterations) {
 			output = Patient.simulationResults(Patient.peripheral, Patient.age, Patient.sex, Patient.diabetes, Patient.afib, Patient.mi, Patient.stroke, Patient.probnp, Patient.egfr, Patient.na, Patient.previousAdm, Patient.sbp, Patient.dbp, Patient.lvef, Patient.hemoglobin);
 			endpointD[i]=output[0];
 			hospitalization[i]=output[1];
 			numHosp[i]=output[2];
+			endpointH[i]=output[3];
+			endpointHind[i]=output[4];
 			i = i + 1;
 		}
 		
 		java.util.Arrays.sort(endpointD);
 		java.util.Arrays.sort(hospitalization);
 		java.util.Arrays.sort(numHosp);
+		
+		// Determine mean values
+		
+		double meanEndpointD = 0;
+		double meanHospitalization = 0;
+		for (int j = 0; j<iterations; j++) {
+			meanEndpointD = meanEndpointD + endpointD[j];
+			meanHospitalization = meanHospitalization + hospitalization[j];
+		}
+		meanEndpointD = meanEndpointD/iterations;
+		meanHospitalization = meanHospitalization/iterations;
 					
-		// Determine survival function
+		// Determine cumulative incidence function of all-cause mortality
 		
 		double[] survProb = new double[7];
-		survProb[0] = 1.0;
+		survProb[0] = 0.0;
 		for (int j = 1; j<7; j++) {
 			
 			boolean notFound = true;
@@ -79,12 +94,30 @@ public class CoachRiskEngine extends JFrame implements ActionListener {
 				if (endpointD[index]>(365/4)*(j+1)) {
 					notFound = false;
 					double interm = (double) (iterations - index);
-					survProb[j] = (int) ((interm/iterations)*100 + 0.5) / 100.0;
+					survProb[j] = 1-((int) ((interm/iterations)*100 + 0.5) / 100.0);
 				}
 				index = index + 1;
 			}
 			
 		}
+		
+		// Determine cumulative incidence function of recurrent hospitalization
+		
+		double[] hospProb = new double[7];
+		for (int z = 0; z<7; z++) {
+			hospProb[z] = 0.0;
+		}
+		int total = 0;
+		for (int j = 1; j<7; j++) {
+			for (int k=0; k<iterations; k++) {
+				if (endpointH[k]<=(365/4)*j & endpointHind[k]==1) {
+					hospProb[j] = hospProb[j]+1;
+					total = total+1;
+				}
+			}
+			hospProb[j] = (int) ((hospProb[j]/iterations)*100 + 0.5) / 100.0;
+		}
+		
 		
 		// Determine histogram
 		
@@ -119,8 +152,10 @@ public class CoachRiskEngine extends JFrame implements ActionListener {
 		histProb[3]=histProb[3]/iterations;
 		histProb[4]=histProb[4]/iterations;
 		
-		results.updateJFreeChartSurvival(survProb);
+		results.updateJFreeChartSurvival(survProb,hospProb);
 		results.updateJFreeChartHistogram(histProb);
+		results.updateDaysLost(java.lang.Math.round(meanHospitalization));
+						
 	}
 	
 	public static double median(int[] m) {
@@ -154,8 +189,8 @@ public class CoachRiskEngine extends JFrame implements ActionListener {
 		
 		if (e.getSource()==aboutItem) {
 			JOptionPane.showMessageDialog(this,
-				"COACH Risk Engine v1.0 \n" +
-				"\uu00a9 2010, Douwe Postmus (d.postmus@epi.umcg.nl) \n \n" + 
+				"COACH Risk Engine v1.1 \n" +
+				"\uu00a9 2010-2011, Douwe Postmus (d.postmus@epi.umcg.nl) \n \n" + 
 				"This software incorporates JFreeChart, \n" + 
 				"\uu00a9 2000-2009 by Object Refinery Limited and Contributors",
 				"About COACH Risk Engine",
